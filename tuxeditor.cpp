a030461e0853e9416144bf8effba8780f57407c8
mehrzero <mehrzero@Gmail.com>
2021-01-28 21:39:37 +0330

    Its skeleton project was created. version Alpha-1  Changes to be committed: 	new file:   AppImages/Add.png 	new file:   AppImages/Address Book.png 	new file:   AppImages/App.png 	new file:   AppImages/AppResources.qrc 	new file:   AppImages/Applications.png 	new file:   AppImages/Apps.png 	new file:   AppImages/Copy.png 	new file:   AppImages/Cut.png 	new file:   AppImages/Del.png 	new file:   AppImages/Delete.png 	new file:   AppImages/Documents.png 	new file:   AppImages/Edit.png 	new file:   AppImages/Export.png 	new file:   AppImages/Favorites New.png 	new file:   AppImages/Favorites.png 	new file:   AppImages/Folder Find.png 	new file:   AppImages/Folder New.png 	new file:   AppImages/Folder.png 	new file:   AppImages/Import.png 	new file:   AppImages/Move.png 	new file:   AppImages/Network App.png 	new file:   AppImages/Network Find.png 	new file:   AppImages/Network.png 	new file:   AppImages/New.png 	new file:   AppImages/Notepad.png 	new file:   AppImages/Open.png 	new file:   AppImages/Paste.png 	new file:   AppImages/Preferences.png 	new file:   AppImages/Redo.png 	new file:   AppImages/Refresh.png 	new file:   AppImages/Save As 2.png 	new file:   AppImages/Save As.png 	new file:   AppImages/Save.png 	new file:   AppImages/Search.png 	new file:   AppImages/Tools.png 	new file:   AppImages/Trash.png 	new file:   AppImages/Undo.png 	new file:   AppImages/Zoom In.png 	new file:   AppImages/Zoom Out.png 	new file:   Build/build-tuxeditor-Desktop_Qt_5_12_9_GCC_64bit-Debug/.qmake.stash 	new file:   Build/build-tuxeditor-Desktop_Qt_5_12_9_GCC_64bit-Debug/moc_predefs.h 	new file:   Build/build-tuxeditor-Desktop_Qt_5_12_9_GCC_64bit-Debug/tuxeditor 	new file:   Build/build-tuxeditor-Desktop_Qt_5_12_9_GCC_64bit-Release/.qmake.stash 	new file:   Build/build-tuxeditor-Desktop_Qt_5_12_9_GCC_64bit-Release/moc_predefs.h 	new file:   Build/build-tuxeditor-Desktop_Qt_5_12_9_GCC_64bit-Release/tuxeditor 	modified:   tuxeditor.cpp 	modified:   tuxeditor.h 	modified:   tuxeditor.pro 	modified:   tuxeditor.ui

--- tuxeditor.cpp
+++ tuxeditor.cpp
@@ -1,11 +1,12 @@
 #include "tuxeditor.h"
 #include "ui_tuxeditor.h"
-
+#include <QFileDialog>
 TuxEditor::TuxEditor(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::TuxEditor)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 TuxEditor::~TuxEditor()
@@ -13,3 +14,101 @@ TuxEditor::~TuxEditor()
     delete ui;
 }
 
+
+void TuxEditor::on_action_New_triggered()
+{
+    mFilename = "";
+    ui->textEdit->setPlainText("NEW");
+}
+
+void TuxEditor::on_action_Open_triggered()
+{
+   QString file = QFileDialog::getOpenFileName(this,"Open a file");
+
+   if(!file.isEmpty()){
+       QFile sFile(file);
+       if(sFile.open(QFile::ReadOnly | QFile::Text)){
+           mFilename = file;
+           QTextStream in(&sFile);
+           QString text = in.readAll();
+           sFile.close();
+
+           ui->textEdit->setPlainText(text);
+
+       }
+   }
+}
+
+void TuxEditor::on_actionSave_triggered()
+{
+    //Text for filename
+    QFile sFile(mFilename);
+    if(sFile.open(QFile::WriteOnly | QFile::Text)){
+        QTextStream out(&sFile);
+
+        out<< ui->textEdit->toPlainText();
+
+        sFile.flush();
+        sFile.close();
+    }
+}
+
+void TuxEditor::on_action_Save_As_triggered()
+{
+    QString file = QFileDialog::getSaveFileName(this,"Open a file");
+    if(!file.isEmpty()){
+        mFilename =file;
+        on_actionSave_triggered();
+    }
+}
+
+void TuxEditor::on_action_Copy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void TuxEditor::on_action_Cut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void TuxEditor::on_action_Undo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void TuxEditor::on_action_Redo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void TuxEditor::on_action_Paste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void TuxEditor::on_action_ZomeIn_triggered()
+{
+    ui->textEdit->zoomIn(1);
+}
+
+void TuxEditor::on_actionZome_Out_triggered()
+{
+    ui->textEdit->zoomOut(1);
+
+}
+
+void TuxEditor::on_action_Exit_triggered()
+{
+
+}
+
+void TuxEditor::on_action_About_triggered()
+{
+    system("firefox https://mehrzero.github.io/");
+}
+
+void TuxEditor::on_action_Del_triggered()
+{
+    ui->textEdit->setText("");
+}
--- tuxeditor.h
+++ tuxeditor.h
@@ -2,6 +2,8 @@
 #define TUXEDITOR_H
 
 #include <QMainWindow>
+#include <QtCore>
+#include <QtGui>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class TuxEditor; }
@@ -15,7 +17,37 @@ public:
     TuxEditor(QWidget *parent = nullptr);
     ~TuxEditor();
 
+private slots:
+    void on_action_New_triggered();
+
+    void on_action_Open_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_action_Save_As_triggered();
+
+    void on_action_Copy_triggered();
+
+    void on_action_Cut_triggered();
+
+    void on_action_Undo_triggered();
+
+    void on_action_Redo_triggered();
+
+    void on_action_Paste_triggered();
+
+    void on_action_ZomeIn_triggered();
+
+    void on_actionZome_Out_triggered();
+
+    void on_action_Exit_triggered();
+
+    void on_action_About_triggered();
+
+    void on_action_Del_triggered();
+
 private:
     Ui::TuxEditor *ui;
+    QString mFilename;
 };
 #endif // TUXEDITOR_H
